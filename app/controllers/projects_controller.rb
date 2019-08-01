c36682eb88d0b79c307acc4817d4b9e990a66676
Iwanami0306 <turmeric.naminami.1073@gmail.com>
2019-08-01 16:22:21 +0900

    project を登録できる

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,5 +1,21 @@
 class ProjectsController < ApplicationController
+  
   def index
     @projects = Project.all
   end
+  def create
+    @project = Project.new(project_params)
+    if @project.save
+      redirect_to projects_index_path
+    end
+  end
+  
+  def new
+    @project=Project.new
+  end
+
+  private
+    def project_params
+      params.require(:project).permit(:owner,:title, :project_status_id)
+    end
 end
